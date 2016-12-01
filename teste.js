2a469a8ba49642c53f0c8d383208444506fcbab2
isabela.angelo <isabela.angelo@gmail.com>
2016-12-01 15:39:14 -0200

    block for sound heard only by the right/left

--- teste.js
+++ teste.js
@@ -1,15 +1,94 @@
 (function(ext) {
+  if (typeof Tone !== 'undefined') {
+		console.log('Tone library is already loaded');
+		startSynth();
+	} else {
+		$.getScript('http://ericrosenbaum.github.io/tone-synth-extension/Tone.min.js', startSynth);
+	}
+  function startSynth() {
+
+    var tone = new Tone();
+    Tone.Transport.start();
+
+    // var quantizeUnit = '@8n';
+    var quantizeUnit = '0';
+
+
+    // effects
+
+    var delay = new Tone.FeedbackDelay(0.25, 0.5);
+    delay.wet.value = 0;
+
+    var pitchShift = new Tone.PitchShift();
+
+    var panner = new Tone.Panner();
+
+    var reverb = new Tone.Freeverb();
+    reverb.wet.value = 0;
+
+    Tone.Master.chain(delay, pitchShift, panner, reverb);
+
+    var soundFileNames = ['meow','boing','this_is_a_test','who_put_the_bomp','cave','drip_drop','drum_machine','eggs','zoop'];
+    var soundSamplers = loadSoundFiles(soundFileNames);
+
+    // polyphonic samplers
+
+    function loadSoundFiles(filenames) {
+        var samplers = [];
+
+        for (var name of filenames) {
+
+            var myVoices = [];
+            for (var i=0; i<6; i++) {
+                var p = new Tone.Sampler('sounds/' + name + '.mp3').toMaster();
+                myVoices.push(p);
+            }
+
+            var polySampler = {
+                voices : myVoices,
+                currentVoice : 0,
+                nextVoice : function() {return this.voices[this.currentVoice++ % this.voices.length];},
+                stopAllVoices : function() {for (var i=0;i<this.voices.length;i++) {this.voices[i].triggerRelease()}},
+            };
+
+            samplers.push(polySampler);
+        }
+
+        return samplers;
+    }
+
+    // should fire once all sounds are loaded - seems to not work
+    Tone.Buffer.onload = function() {
+        console.log('loaded audio samples');
+    };
+
+
     // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
+    ext._shutdown = function() {
+      stopAllSounds();
+    };
+
+    function stopAllSounds() {
+			synth.triggerRelease();
+		}
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+      if (typeof AudioContext !== "undefined") {
+      return {status: 2, msg: 'Ready'};
+    } else {
+      return {status: 1, msg: 'Browser not supported'};
+    }
     };
 
     ext.my_first_block = function() {
         // Code that gets executed when the block is run
+        panner.pan.value = 1;
+
+        soundSamplers[e.data.soundnum].nextVoice().triggerAttack(0, quantizeUnit);
+
+
     };
 
     // Block and block menu descriptions
@@ -21,5 +100,7 @@
     };
 
     // Register the extension
-    ScratchExtensions.register('My first extension', descriptor, ext);
+    ScratchExtensions.register('Sounds', descriptor, ext);
+
+  };
 })({});
